26760093872815ee6adf940313af249435cc3b86
TrishaD <TrishaD@LPT290-M1.quinnox.corp>
2020-06-20 20:37:54 +0530

    Test GIT

---
+++ VimalT/src/SimpleOOP/TestGIT.java
@@ -0,0 +1,12 @@
+package SimpleOOP;
+
+public class TestGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GIT is Magic");
+
+	}
+
+}
