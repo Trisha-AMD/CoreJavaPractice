7e6529cc2af318a6ce3d32618866d3920d57bc0c
TrishaD <TrishaD@LPT290-M1.quinnox.corp>
2020-06-21 19:01:03 +0530

    test

---
+++ VimalT/src/AllString/DontPanic.java
@@ -0,0 +1,11 @@
+package AllString;
+
+public class DontPanic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Everything is gonna be alright");
+
+	}
+
+}
