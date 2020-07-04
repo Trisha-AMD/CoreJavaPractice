8c1e0f610299f728fc9079ebfd18e200d506478c
TrishaD <TrishaD@LPT290-M1.quinnox.corp>
2020-07-04 12:31:06 +0530

    New changes

--- VimalT/src/AllString/DontPanic.java
+++ VimalT/src/AllString/DontPanic.java
@@ -5,6 +5,8 @@ public class DontPanic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Everything is gonna be alright");
+		
+		System.out.println("We are panicking now");
 
 	}
 
